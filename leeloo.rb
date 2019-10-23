3f4dce62127466f8314c49737776fb7a16f54f52
Sylvain M <smaucourt@gmail.com>
2019-10-23 15:37:14 +0200

    adds gpgme depends_on.

--- leeloo.rb
+++ leeloo.rb
@@ -5,6 +5,7 @@ class Leeloo < Formula
   sha256 "15770409158d13f3184e21dd23f8d6c281f309d3c4779d2e5cf249bc1f9455f3"
 
   depends_on "gnupg"
+  depends_on "gpgme"
   depends_on "ruby"
   depends_on "ruby-build"
 
